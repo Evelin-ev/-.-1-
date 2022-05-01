b2e8a4f27bf9454cfcd63abecb86c6e612287104
Evelina <evelina.evish@yandex.ru>
2022-05-01 12:45:06 +0300

    Создала папку Задача_2.1, написала программу поиска большего числа

---
+++ Задача_2.1/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Написать программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее */
+
+
+
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if(number2 > max) max = number2;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+
