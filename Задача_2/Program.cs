c27f34ad47fb0064e83aa9607827cf5bbfc5d377
Evelina <evelina.evish@yandex.ru>
2022-05-01 12:58:08 +0300

    Отредактировала Задачу_2, добавила min

--- Задача_2/Program.cs
+++ Задача_2/Program.cs
@@ -8,9 +8,13 @@ Console.WriteLine("Введите второе число");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
 int max = number1;
+int min = number2;
 
 if(number2 > number1) max = number2;
+if(number2 > number1) min = number1;
 
 Console.Write("большее число = ");
 Console.WriteLine(max);
 
+Console.Write("меньшее число = ");
+Console.WriteLine(min);
