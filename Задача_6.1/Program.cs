bf0a842388fa6b9acda6e6b48f898ae4156841b4
Evelina <evelina.evish@yandex.ru>
2022-05-01 13:47:22 +0300

    Создала папку Задача_6.1, вывод рандомного числа и определение чутное оно или нет

---
+++ Задача_6.1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая на вход принимает число (рандомное) и выдает, является ли число чётным (делится ли оно на два без остатка)
+
+
+
+int number = new Random().Next(0, 1000);
+Console.WriteLine(number);
+
+if(number % 2 == 0)
+{
+    Console.WriteLine("Четное число");  
+} 
+if(number % 2 != 0)
+{
+    Console.WriteLine("Нечетное число");
+}
