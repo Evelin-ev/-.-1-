fc7b7c2daa77e160c75163f8bfbe7ce5c71cfeb7
Evelina <evelina.evish@yandex.ru>
2022-05-01 12:50:46 +0300

    Создала папку Задача_2.2, нахождение максимального рандомного числа

---
+++ Задача_2.2/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Написать программу, которая на вход принимает два числа и выдает (рандомные числа), какое число большее, а какое меньшее */
+
+
+
+int number1 = new Random().Next(0, 1000000);
+Console.WriteLine(number1);
+int number2 = new Random().Next(0, 1000000);
+Console.WriteLine(number2);
+int max = number1;
+
+if(number2 > max) max = number2;
+
+Console.Write("max = ");
+Console.WriteLine(max);
