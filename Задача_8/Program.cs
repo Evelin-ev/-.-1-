5b8370f1fe3a7c8753c8448b30b0638517e7bb0a
Evelina <evelina.evish@yandex.ru>
2022-05-01 14:58:08 +0300

    Создала папку Задача_8, вывод на экран всеч четных чисел введенного числа

---
+++ Задача_8/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая на вход принимает число(N), а на выходе показывает все чётные числа от 0 до N.
+
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int temp = number - 1;
+if(number > 0)
+{
+    for (int i = 1; i <= number; i++)
+    {
+    if (i % 2 == 0)
+    {
+        Console.Write(i);
+    if (i < temp)
+    {
+        Console.Write(" , ");
+    }
+    }
+    }
+}
+else
+{
+    Console.WriteLine("Введено некоректное число!");
+}
