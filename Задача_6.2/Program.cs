d9525aed2ae6f65a33b65c1e2af334a2b56fd0cd
Evelina <evelina.evish@yandex.ru>
2022-05-01 13:51:08 +0300

    Создала папку Задача_6.2, улучшиный вариант вывода на экран информации о четности или нечетности числа

---
+++ Задача_6.2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа, которая на вход принимает число и выдает, является ли число чётным (делится ли оно на два без остатка)
+
+
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 == 0)
+{
+    Console.Write(number);
+    Console.WriteLine(" - четное число");  
+} 
+if(number % 2 != 0)
+{
+    Console.Write(number);
+    Console.WriteLine(" - нечетное число");
+}
